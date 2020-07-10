57808932514ad4947d275c247db7369d66aa8d85
suhel hasan <suhelhasan202@gmail.com>
2020-07-10 13:40:17 +0530

    changing the logo
    Signed-off-by: suhel hasan <suhelhasan202@gmail.com>

--- src/UI/Footer/Footer.js
+++ src/UI/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styling from "./Footer.module.css";
-import logo from "../../images/logo.png";
+import logo from "../../images/logo1.png";
 
 export default function Footer() {
   return (
